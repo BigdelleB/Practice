8ddb2465aeef3007072ab72e3fb616c21bfc8fba
Brian Bigdelle <bb586@cornell.edu>
2015-02-25 18:48:27 -0500

    2nd commit

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Practice using GitHub commands, pure testing purposes"
+
+println(str)
+
+for index in 1...5{
+    println(str)
+}
+
